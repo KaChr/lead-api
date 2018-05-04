33c0b3cd6d4bb06274c02c3259783b85c1063aad
Ulf Zackrisson <ulmezz@gmail.com>
2018-05-04 10:41:17 +0200

    Deleted commented lines in model listings

--- src/models/listings.js
+++ src/models/listings.js
@@ -1,21 +1,4 @@
 'use strict';
-/*module.exports = (sequelize, DataTypes) => {
-  var Listings = sequelize.define('Listings', {
-    title: DataTypes.STRING(50),
-    pub_date: DataTypes.DATEONLY,
-    information: DataTypes.TEXT,
-    intern_amount: DataTypes.INTEGER,
-    profile_company_id: DataTypes.INTEGER,
-    timestamp: DataTypes.DATE
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return Listings;
-};*/
 
 export default (sequelize, DataTypes) => {
   const Listings = sequelize.define('Listings', {
