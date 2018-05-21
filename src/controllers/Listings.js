af695747b34f784bafe9e7924732e2f0c6b8aa9e
KaChr <karin.christensen@chasacademy.se>
2018-05-21 14:18:25 +0200

    Remove unnecessary comments

--- src/controllers/Listings.js
+++ src/controllers/Listings.js
@@ -1,42 +1,4 @@
-// import * as Lists from '../lib/Lists'
 import DB from '../models'
-
-//export default {
-    /*list(req, res) {
-        res.status(200).json({
-            data: res
-        })
-    }*/
-
-/*module.exports = Listings.findAll()
-.then(function(listings) {
-    console.log(listings);
-    //res.send({error:false,message:'users list',data:users});
-})
-.catch(function(err) {
-    console.log('Oops! something went wrong, : ', err);
-})*/
-//}
-
-/*module.export = function list(req, res, next) {
-    Listings.findAll()
-    .then( function(userResponse) {
-        res.status(200).json({
-            data: userResponse
-        })
-    })
-    .catch(function(error) {
-        res.status(400).json({
-            message:error
-        })
-    })
-}*/
-/*DB.Listings.findAll({
-    include: [{
-        model: DB.Companies,
-        where: { id: Sequelize.col('Listings.company_id') }
-    }]
-}) */
 
 
 export default {
@@ -80,24 +42,6 @@ export default {
         .catch((error) => {
             res.status(400).send(error);
         });
-        
-        
-       // .then(function(data) {
-         //   data.map((list) => {
-           //     DB.Companies.find(
-             //       {where: {
-               //         company_id: list.company_id
-                 //   }}   
-            //    )
-           //     .then(function(data) {
-
-             //   })
-               // res.status(200).send(data);
-          //  })
-        //})
-        /*.catch(function(error) {
-          res.status(400).send(error);
-        })*/
     },
     find(req, res) {
         DB.Listings.findById(req.params.id)
