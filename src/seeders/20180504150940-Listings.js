912ad0553db5367d4ac1e299e0faaa9fc5092a4a
christoffer malmgren <christoffer.malmgren@chasacademy.se>
2018-05-24 20:08:45 +0200

    changed seeder in listings

--- src/seeders/20180504150940-Listings.js
+++ src/seeders/20180504150940-Listings.js
@@ -3,54 +3,54 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
        return queryInterface.bulkInsert('Listings', [{
-        title: 'company1',
+        title: 'Backend',
         pub_date: new Date(),
-        information_listing: 'bla bla ba 1. bla bla ba 1. bla bla ba 1. bla bla ba 1. bla bla ba 1. bla bla ba 1.',
+        information_listing: 'söker java..',
         intern_amount: 3,
         company_id: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       { 
-        title: 'company2',
+        title: 'Frontend',
         pub_date: new Date(),
-        information_listing: 'bla bla ba 2. bla bla ba 2. bla bla ba 2. bla bla ba 2. bla bla ba 2. bla bla ba 2.',
+        information_listing: 'duktig fr..',
         intern_amount: 3,
         company_id: 2,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       { 
-        title: 'company3',
+        title: 'Javascript',
         pub_date: new Date(),
-        information_listing: 'bla bla ba 3. bla bla ba 3. bla bla ba 3. bla bla ba 3. bla bla ba 3. bla bla ba 3.',
+        information_listing: 'sökes..',
         intern_amount: 2,
         company_id: 3,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       { 
-        title: 'company4',
+        title: 'Angular',
         pub_date: new Date(),
-        information_listing: 'bla bla ba 4. bla bla ba 4. bla bla ba 4. bla bla ba 4. bla bla ba 4. bla bla ba 4.',
+        information_listing: 'snabb utv..',
         intern_amount: 5,
         company_id: 4,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       { 
-        title: 'company5',
+        title: 'React',
         pub_date: new Date(),
-        information_listing: 'bla bla ba 5. bla bla ba 5. bla bla ba 5. bla bla ba 5. bla bla ba 5. bla bla ba 5.',
+        information_listing: 'Stresstålig..',
         intern_amount: 23,
         company_id: 1,
         createdAt: new Date(),
         updatedAt: new Date()
       },
       { 
-        title: 'company6',
+        title: 'NodeJS',
         pub_date: new Date(),
-        information_listing: 'bla bla ba 6. bla bla ba 6. bla bla ba 6. bla bla ba 6. bla bla ba 6. bla bla ba 6.',
+        information_listing: 'Grym back..',
         intern_amount: 9,
         company_id: 3,
         createdAt: new Date(),
